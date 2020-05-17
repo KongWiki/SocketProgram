e1d878739264971db0a7c5e06d13ec1e34150985
KongWiki <kongwiki@163.com>
2020-05-17 16:29:42 +0800

    init

---
+++ src/main/java/com/wkk/Client.java
@@ -0,0 +1,15 @@
+package com.wkk;
+
+import java.net.Socket;
+
+/**
+ * @Time: 2020/5/17下午2:53
+ * @Author: kongwiki
+ * @Email: kongwiki@163.com
+ */
+public class Client {
+    public static void main(String[] args) {
+        Socket socket = new Socket();
+
+    }
+}
---
+++ src/main/java/com/wkk/Server.java
@@ -0,0 +1,9 @@
+package com.wkk;
+
+/**
+ * @Time: 2020/5/17下午2:53
+ * @Author: kongwiki
+ * @Email: kongwiki@163.com
+ */
+public class Server {
+}
