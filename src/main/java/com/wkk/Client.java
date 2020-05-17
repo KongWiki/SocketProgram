a2c2bf4e133fc273bb821c9c2e825cb35c930387
KongWiki <kongwiki@163.com>
2020-05-17 20:04:21 +0800

    基础的socket通信

--- src/main/java/com/wkk/Client.java
+++ src/main/java/com/wkk/Client.java
@@ -1,5 +1,6 @@
 package com.wkk;
 
+import java.io.*;
 import java.net.Socket;
 
 /**
@@ -8,8 +9,36 @@ import java.net.Socket;
  * @Email: kongwiki@163.com
  */
 public class Client {
+
     public static void main(String[] args) {
-        Socket socket = new Socket();
+        final String DEFAULT_SERVER_HOST = "localhost";
+        final String QUIET = "quiet";
+        final int DEFAULT_SERVER_PORT = 8888;
+        try (
+                Socket client = new Socket(DEFAULT_SERVER_HOST, DEFAULT_SERVER_PORT);
+                // 创建IO流
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(client.getInputStream()));
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(client.getOutputStream()));
+
+                // 等待用户输入信息
+                BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+        ) {
+
+            // 获取输入信息
+            String input = consoleReader.readLine();
+
+            // 发送给服务器
+            writer.write(input + "\n");
+            writer.flush();
+            // 获取服务器数据
+            String accept = reader.readLine();
+            System.out.println("[服务器 发送数据 " + accept + "]");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
--- src/main/java/com/wkk/Server.java
+++ src/main/java/com/wkk/Server.java
@@ -1,9 +1,54 @@
 package com.wkk;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * @Time: 2020/5/17下午2:53
  * @Author: kongwiki
  * @Email: kongwiki@163.com
  */
 public class Server {
+    public static void main(String[] args) {
+        final int DEFAULT_PORT = 8888;
+        final String QUIET = "quiet";
+        ServerSocket server = null;
+        try {
+            server = new ServerSocket(DEFAULT_PORT);
+            System.out.println("[启动服务器， 监听端口 " + DEFAULT_PORT
+                    + "]");
+            while (true) {
+                // 等待链接
+                Socket client = server.accept();
+                System.out.println("[客户端 " +
+                        client.getPort() + " 已经链接]");
+
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(client.getInputStream()));
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(client.getOutputStream()));
+
+                // 读取客户端数据
+                String msg = reader.readLine();
+                if (!msg.equalsIgnoreCase(QUIET)) {
+                    System.out.println("[客户端 " + client.getPort() + "发送数据 " + msg + "]");
+                    // 返回个客户端数据
+                    writer.write("echo " + msg + "\n");
+                    writer.flush();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (server != null) {
+                try {
+                    server.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 }
